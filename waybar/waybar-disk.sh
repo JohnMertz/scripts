c0592df770d51b4ee87a1e0fc29db61072d08c02
John Mertz <git@john.me.tz>
2026-04-24 22:22:59 -0600

    clean up notification columns

--- waybar/waybar-disk.sh
+++ waybar/waybar-disk.sh
@@ -6,4 +6,4 @@ notify-send Disks \
   "$(lsblk -o NAME,SIZE,FSUSE%,MOUNTPOINT | grep -vP '^loop' |
     sed 's/MOUNTPOINT/MOUNT/' | sed -e 's/│ └─/+---/' |
     sed -e 's/  └─/+---/' | sed -e 's/├─/+-/' | sed -e 's/└─/+-/' |
-    awk {'printf "%-20s %-7s %-    6s %-7s\n", substr($1, 1, 20), $2, $3, $4'})"
+    awk {'printf "%-12s %-7s %-    6s %-7s\n", substr($1, 1, 12), $2, $3, $4'})"
--- waybar/waybar-mem.sh
+++ waybar/waybar-mem.sh
@@ -2,6 +2,4 @@
 
 # Create a reasonably well organized notification with memory usage stats
 
-notify-send -t 0 'Memory Usage' "$(free -h |
-  awk '{printf(\"%6s %6s %6s %6s\n\", $1, $2, $3, $4)}' |
-  sed -r 's/(.*) shared$/       \1/')"
+notify-send -t 0 'Memory Usage' "$(free -h | awk '{ printf("%6s %6s %6s %6s\n",$1,$2,$3,$4) }' | sed -r 's/(.*) shared$/       \1/')"
